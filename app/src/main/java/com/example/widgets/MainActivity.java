25ee6142f8bd3da0ea74297d1966d4c86d179545
d22marwi <d22marwi@student.his.se>
2024-04-10 22:14:06 +0200

    Jag har lagt till en horizontal layout.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.widgets;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     @Override
